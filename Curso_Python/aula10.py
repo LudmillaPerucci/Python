ebd32b4ab764fc6aa51a40f0722892e4c497f9f4
Ludmilla Perucci <85947891+LudmillaPerucci@users.noreply.github.com>
2024-12-06 12:49:43 -0300

    Add files via upload

---
+++ Curso_Python/aula10.py
@@ -0,0 +1,7 @@
+concatenacao = 'Ludmilla' + ' ' + 'Perucci'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_lud = 3 * 'Ludmilla'
+print(a_dez_vezes)
+print(tres_vezes_lud)
---
+++ Curso_Python/aula12.py
@@ -0,0 +1,9 @@
+nome = 'Ludmilla Perucci'
+altura = 1.56
+peso = 56
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
